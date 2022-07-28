da76697ffadb08bbf2e848f3249c818a8eea8f97
Alex <alexbachurin91@gmail.com>
2022-07-28 15:18:39 +0300

    get categoryPage collection by using query with variables by passing categoryName

--- src/pages/Category/CategoryPage.js
+++ src/pages/Category/CategoryPage.js
@@ -1,31 +1,53 @@
 import React, { useState, useEffect } from "react";
 import Wrapper from "./Wrapper";
 import { useParams } from "react-router-dom";
-import { useCategoriesContext } from "../../contexts/categoriesContext";
 import ProductCard from "../../components/ProductCard/ProductCard";
+import { gql, useQuery } from "@apollo/client";
+
+//query to get single collection by title
+const GET_CATEGORY = gql`
+	query ($title: String!) {
+		getCollectionsByTitle(title: $title) {
+			id
+			title
+			items {
+				id
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
+
 const CategoryPage = () => {
 	//get target url with clicked category name using useParams
 	const { categoryName } = useParams();
-	const { categories } = useCategoriesContext();
-	//state for storing products
+	//state for storing products for single item
 	const [products, setProducts] = useState([]);
-	//every time categoryName or categories from db changes call useEffect to rerender page
+	//get single collection by using query and passing title of clicked category name to it
+	const { loading, data, error } = useQuery(GET_CATEGORY, {
+		variables: {
+			title: categoryName,
+		},
+	});
+	//every time categoryName or data from db changes call useEffect to rerender page
 	useEffect(() => {
-		//first find in categories array item with matching category name
-		const filteredArr = categories.filter((item) => {
-			return item.title.toLowerCase() === categoryName.toLowerCase();
-		});
-		//then set items of found matching categories items to products
-		setProducts(filteredArr[0].items);
-	}, [categoryName, categories]);
+		if (data) {
+			const items = data.getCollectionsByTitle.items;
+			setProducts(items);
+		}
+	}, [categoryName, data]);
 	return (
 		<Wrapper>
 			<h2 className="category-title">{categoryName}</h2>
 			<div className="category-items-container">
-				{products &&
-					products.map((product) => {
-						return <ProductCard key={product.id} {...product} />;
-					})}
+				{loading
+					? "Loading..."
+					: products &&
+					  products.map((product) => {
+							return <ProductCard key={product.id} {...product} />;
+					  })}
 			</div>
 		</Wrapper>
 	);
