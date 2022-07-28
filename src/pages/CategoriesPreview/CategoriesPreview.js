9f1b95d480c8999256496269da49558ad3f22a02
Alex <alexbachurin91@gmail.com>
2022-07-28 15:00:08 +0300

    added loading to display

--- src/pages/CategoriesPreview/CategoriesPreview.js
+++ src/pages/CategoriesPreview/CategoriesPreview.js
@@ -3,18 +3,18 @@ import { useCategoriesContext } from "../../contexts/categoriesContext";
 import CategoryPreview from "../../components/CategoryPreview/CategoryPreview";
 import Wrapper from "./Wrapper";
 const CategoriesPreview = () => {
-	const { categories } = useCategoriesContext();
+	const { categories, loading } = useCategoriesContext();
 
 	return (
 		<Wrapper>
-			{
-				// use object keys to get the title from categories object
-				//and pass to CategoryPreview component title and associated products with this title
-				categories.map((item) => {
-					const { title, items } = item;
-					return <CategoryPreview key={title} title={title} products={items} />;
-				})
-			}
+			{loading
+				? "Loading..."
+				: categories.map((item) => {
+						const { title, items } = item;
+						return (
+							<CategoryPreview key={title} title={title} products={items} />
+						);
+				  })}
 		</Wrapper>
 	);
 };
